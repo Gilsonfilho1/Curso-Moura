ac8430f01a4153cf7dedc61293cd6797001dfabb
Gilson Filho <farofalocona250604@outlook.com>
2025-08-28 09:37:18 -0300

    Main

---
+++ Logica/Calculadora/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("Digite o primeiro número:");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A soma dos números é: {0}", n1 + n2);
+            Console.WriteLine("A Subtração dos números é: {0}", n1 - n2);
+            Console.WriteLine("A Multiplicação dos números é: {0}", n1 * n2);
+            Console.WriteLine("A Divisão dos números é: {0}", n1 / n2);
+            Console.WriteLine("O Resto da Divisão dos números é: {0}", n1 % n2);
+
+
+            Console.WriteLine("Pressione ENTER para encerrar o programa!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Logica/Numeros/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Numeros
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("{1} {0} ", n1 * 2, "Dobro");
+            Console.WriteLine("Pressione ENTER para encerrar o programa!");
+            Console.ReadLine();
+        }
+    }
+}
